2bec12e72edeb3732ebb285a034303ca1f858f45
Jorge <juniorvalente2014@gmail.com>
2020-06-08 06:38:00 -0300

    finished jogo_do_nim

--- week6/game/jogo_do_nim.py
+++ week6/game/jogo_do_nim.py
@@ -1,11 +1,15 @@
 def inicio():
     print('Bem vindo ao jogo do NIM! Escolha:\n')
-    modo_de_jogo = int(input('1 - para jogar uma partida isolada\n2 - para jogar um campeonato '))
+    modo_de_jogo = int(
+        input(
+            '1 - para jogar uma partida isolada\n2 - para jogar um campeonato '
+        ))
     if modo_de_jogo == 1:
-          print('\nvocê escolheu uma partida isolada!\n')
-          partida()
+        print('\nvocê escolheu uma partida isolada!\n')
+        partida()
     else:
         campeonato()
+
 
 def campeonato():
     rodada = 1
@@ -20,18 +24,15 @@ def campeonato():
     print('\n**** final do campeonato! ****')
     print('\nplacar: você 0 x 3 computador')
 
+
 def partida():
     n = int(input('Quantas peças? '))
     m = int(input('Limite de peças por jogada? '))
-    #define quem vai executar a primeira jogada
 
+    #define quem vai executar a primeira jogada
     if n % (m + 1) == 0:
         print('\nVocê começa!')
-        retirada = n
-        while retirada == 0 or retirada > m or retirada < 0:
-            retirada = usuario_escolhe_jogada(n, m)
-            if retirada == 0 or retirada > m or retirada < 0:
-                print('Oops! Jogada inválida! Tente de novo.')
+        retirada = usuario_escolhe_jogada(n, m)
         n -= retirada
         print(f'agora restam {n} peças no tabuleiro')
         vez = 'computador'
@@ -40,11 +41,12 @@ def partida():
         print('\nComputador começa!')
         retirada = computador_escolhe_jogada(n, m)
         n -= retirada
-
         print(f'\no computador tirou {retirada} peças')
         print(f'agora restam {n} peças no tabuleiro')
+
         if n == 0:
             print('\nFim do Jogo! O computador ganhou!')
+
         vez = 'usuario'
 
     while n != 0:
@@ -60,14 +62,12 @@ def partida():
 
             vez = 'usuario'
         else:
-            retirada = n
-            while retirada == 0 or retirada > m or retirada < 0:
-                retirada = usuario_escolhe_jogada(n, m)
-                if retirada == 0 or retirada > m or retirada < 0:
-                    print('\nOops! Jogada inválida! Tente de novo.')
+            retirada = usuario_escolhe_jogada(n, m)
             n -= retirada
             print(f'agora restam {n} peças no tabuleiro')
+
             vez = 'computador'
+
 
 def computador_escolhe_jogada(n, m):
     computador_retirou = n
@@ -80,7 +80,8 @@ def computador_escolhe_jogada(n, m):
         computador_retirou = m
         computador_return = computador_retirou
     else:
-        while computador_retirou >= m and not computador_retirou % (m + 1) == 0:
+        while computador_retirou >= m and not computador_retirou % (
+                m + 1) == 0:
             computador_retirou -= 1
         computador_return = n - computador_retirou
 
@@ -89,11 +90,13 @@ def computador_escolhe_jogada(n, m):
 
     return computador_return
 
+
 def usuario_escolhe_jogada(n, m):
     jogador_retirou = int(input('\nQuantas peças você vai tirar? '))
     while jogador_retirou > m or jogador_retirou <= 0:
-        print('Oops! Jogada inválida! Tente de novo.')
+        print('\nOops! Jogada inválida! Tente de novo.')
         jogador_retirou = int(input('\nQuantas peças você vai tirar? '))
     return jogador_retirou
+
 
 inicio()
--- week6/game/temp.py
+++
@@ -1,7 +0,0 @@
-from funPartida import computador_escolhe_jogada
-from temp3 import usuario_escolhe_jogada
-from funPartida import partida
-from funPartida import campeonato
-from funPartida import inicio
-
-inicio()
